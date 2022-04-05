c4cd6dea8ba5fd46819693407bfa7f687b456e55
jdhao <jdhao@hotmail.com>
2022-04-05 20:23:24 +0800

    first workable version

--- run-sgd.py
+++ run-sgd.py
@@ -1,6 +1,9 @@
-import numpy as np
 import random
+
+import numpy as np
+from numpy.linalg import norm
 from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
 
 
 def compute_accuracy(preds, gt):
@@ -28,35 +31,96 @@ class SGDClassifier:
         self.n_cls = n_cls
         self.use_bias = use_bias
 
-    def set_params(self, batch_size=32, lr=0.01, dropout=True, n_epocs=10, n_iters=1000):
+    def set_params(self, batch_size=32, lr=0.01, dropout=False, n_epochs=200, shuffle=True,
+                   weight_penalty=True, alpha=0.001):
+
+        # number of epochs to train
+        self.n_epochs = n_epochs
+
+        # whether to shuffle data after each epoch
+        self.shuffle = shuffle
 
         # training hyper parameters
         self.batch_size = batch_size
         self.lr = lr
+
+        # whether to use feature dropout
         self.dropout = dropout
-        # number of epocs, number of iterations per epoc
-        self.n_epocs = n_epocs
-        self.n_iter = n_iters
 
-    def fit(self, x, y):
+        # whether to add weight regularization
+        self.weight_penalty = weight_penalty
 
-        pass
+        # weight penalty term if regularization is used, aka, weight decay
+        self.alpha = alpha
+
+    def fit(self, xtrain, ytrain, xval, yval):
+        # train the model for n_epochs
+        for i in range(self.n_epochs):
+            train_loss = 0
+            n_iter = 0
+            for x_batch, y_batch in self.__get_batch(xtrain, ytrain):
+
+                n_iter += 1
+                train_loss_batch = self.__compute_loss(x_batch, y_batch)
+                train_loss += train_loss_batch
+
+                # compute and update gradient
+                self.__compute_gradient(x_batch, y_batch)
+
+            val_loss = self.__compute_loss(xval, yval)
+            val_acc = compute_accuracy(self.predict(xval), yval)
+
+            print(f"epoch: {i}, train loss: {train_loss/n_iter}, val loss: {val_loss}, " +
+                  f"val acc: {val_acc}")
+
+
+    def __get_batch(self, x, y):
+        n_samples = x.shape[0]
+        x, y = x.copy(), y.copy()
+
+        idx = list(range(n_samples))
+        # shuffle the data
+        if self.shuffle:
+            random.shuffle(idx)
+
+        # get a batch of data
+        for i in range(0, n_samples, self.batch_size):
+            batch_idx = idx[i:i+self.batch_size]
+            yield x[batch_idx], y[batch_idx]
+
 
     def predict(self, x):
+        # probability output
+        prob = 1 / (np.exp(-1* (np.dot(self.w, x.T) + self.b)) + 1)
 
-        return random.choices([0, 1], k=x.shape[0])
+        y_pred = []
+        for p in prob:
+            if p <= 0.5:
+                y_pred.append(0)
+            else:
+                y_pred.append(1)
+
+        return y_pred
+
 
     def __compute_loss(self, x, y):
+        prob = 1 / (np.exp(-1* (np.dot(self.w, x.T) + self.b)) + 1)
+        # print(f"prob: {prob}")
+        loss = -np.mean(y * np.log(prob) + (1-y) * np.log(1-prob)) + 0.5 * self.alpha * norm(self.w)
 
-        # compute log-loss
+        return loss
 
-        return 0.0
-
-    def __compute_gradient(self, loss, x, y):
-
+    def __compute_gradient(self, x, y):
         # compute gradient over the batch
+        prob = 1 / (np.exp(-1* (np.dot(self.w, x.T) + self.b)) + 1)
 
-        return 0.0
+        n = x.shape[0]
+        delta_w = 1.0 / n * np.dot((prob - y).T, x) + self.alpha * self.w
+        delta_b = np.mean(prob - y)
+
+        # update parameters
+        self.w -= self.lr * delta_w
+        self.b -= self.lr * delta_b
 
 
 def run_training():
@@ -67,12 +131,17 @@ def run_training():
 
     print(f"N={N}, D={D}")
 
+    # normalize the features
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
+
     # setup classifier
     cls = SGDClassifier(dim=D)
     cls.set_params(batch_size=16, lr=0.001)
 
     # fit data to model
-    cls.fit(data, labels)
+    cls.fit(data, labels, data, labels)
+
     preds = cls.predict(data)
 
     acc = compute_accuracy(preds, labels)
